e3b163a07c7615bd8959bcb76d0331c8804dd70e
ieow <cwlee0@gmail.com>
2024-02-05 14:10:11 +0800

    fix: add types

---
+++ packages/base/dist/types/provider/IProvider.d.ts
@@ -0,0 +1,5 @@
+export type { JRPCRequest, JRPCResponse, Maybe, RequestArguments, SafeEventEmitter, SafeEventEmitterProvider, SendCallBack, } from "@toruslabs/openlogin-jrpc";
+export declare const PROVIDER_EVENTS: {
+    INITIALIZED: string;
+    ERRORED: string;
+};
